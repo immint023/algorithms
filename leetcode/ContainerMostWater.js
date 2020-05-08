c5b2e62ba2dafb997bee7180d850fc14f2a21b5b
Ngo Minh <nvminh023@gmail.com>
2020-05-08 22:34:53 +0700

    Add reference link

--- leetcode/ContainerMostWater.js
+++ leetcode/ContainerMostWater.js
@@ -1,27 +1,24 @@
+// ref: https://leetcode.com/problems/container-with-most-water/solution/
 /**
  * @param {number[]} height
  * @return {number}
  */
+
 var maxArea = function(height) {
-  let i = 0;
-  let j = height.length - 1;
-  let maxL = height[i];
-  let maxR = height[j];
-  let area = 0;
-  let max = 0;
-  while (i < j) {
-      if (maxL <= maxR) {
-          area = maxL * (j -i);
-          max = Math.max(area, max);
-          i++
-          maxL = height[i];
-          
-      } else {
-          area = maxR * (j -i)
-          max = Math.max(area, max);
-          j--;
-          maxR = height[j];
-      }
-  }
-  return max;
+    let result = 0;
+    let i = 0;
+    let j = height.length - 1;
+    while (i < j) {
+        if (height[i] < height[j]) {
+            const water = height[i] * (j - i);
+            if (water > result) result = water;
+            ++i;
+        } else {
+            const water = height[j] * (j - i);
+            if (water > result) result = water;
+            --j;
+        }
+    }
+    return result;
 };
+
