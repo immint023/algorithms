9fdd9f92ff9901223b3e99c7b0c9d00c0a6b87e0
Ngo Minh <nvminh023@gmail.com>
2020-09-17 23:17:53 +0700

    Combine sum

---
+++ leetcode/combination-sum.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ *
+ * reference: https://leetcode.com/problems/combination-sum/submissions/
+ */
+
+var combinationSum = function (candidates, target) {
+  var results = [];
+  var array = [];
+  var sumValue = 0;
+
+  var helper = (index) => {
+    for (let i = index; i < candidates.length; ++i) {
+      var item = candidates[i];
+
+      array.push(item);
+
+      sumValue += item;
+
+      if (sumValue === target) {
+        results.push([...array]);
+      }
+
+      if (sumValue < target) {
+        helper(i);
+      }
+
+      var poppedItem = array.pop();
+      sumValue -= poppedItem;
+    }
+  };
+
+  helper(0);
+
+  return results;
+};
